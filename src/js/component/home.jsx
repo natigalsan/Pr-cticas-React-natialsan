325af7f147fc4ae7359e3d40ca886762dbc8319e
natigalsan <natividadgalsan@gmail.com>
2022-04-13 19:42:14 +0000

    3modificacion

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,29 +3,31 @@ import React from "react";
 //include images into your bundle
 
 //create your first component
-import Card from "./card.jsx"; 
+import Card from "./card.jsx";
+import NavBar from "./navbar.jsx";
+import Header from "./header.jsx";
 const Home = () => {
 	return (
 		<>
-			
-			<Card src ="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
-				titulo = "Paisaje de Japón"
-				text = "Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
-				/>
-			<Card src ="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
-				titulo = "Paisaje de Japón"
-				text = "Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
-				/>
-			<Card src ="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
-				titulo = "Paisaje de Japón"
-				text = "Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
-				/>
+			<NavBar />
 
-						
+			<Card
+				src="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
+				titulo="Paisaje de Japón"
+				text="Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
+			/>
+			<Card
+				src="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
+				titulo="Paisaje de Japón"
+				text="Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
+			/>
+			<Card
+				src="https://us.123rf.com/450wm/pitinan/pitinan1904/pitinan190404410/121655857-hermosa-ma%C3%B1ana-en-la-pagoda-yasaka-y-sannen-zaka-street-en-verano-kyoto-jap%C3%B3n-la-pagoda-de-yasaka-es.jpg?ver=6"
+				titulo="Paisaje de Japón"
+				text="Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual ..."
+			/>
 		</>
 	);
 };
-
-
 
 export default Home;
